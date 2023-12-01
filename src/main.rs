f293cd996aa75957b9ff75e188d9f492b159bf66
Aoyama, Shotaro <aoyama@val.co.jp>
2023-12-01 09:32:26 +0900

    画面表示できた

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,66 @@
-fn main() {
-    println!("Hello, world!");
+extern crate sdl2;
+
+use sdl2::event::Event;
+use sdl2::keyboard::Keycode;
+use sdl2::pixels::Color;
+use std::time::Duration;
+
+const SCREEN_WIDTH: u32 = 640;
+const SCREEN_HEIGHT: u32 = 420;
+const CELL_SIZE_PX: u32 = 20;
+
+// SCREEN_WIDTH = 640
+// SCREEN_HEIGHT = 420
+// CELL_SIZE_PX = 20
+// BACKGROUND_COLOR = [0, 0, 0]
+// WALL_COLOR = [128, 128, 128]
+// BLOCK_COLORS = [
+//   [255, 0, 0],
+//   [0, 255, 0],
+//   [0, 0, 255],
+//   [255, 255, 0],
+//   [0, 255, 255],
+//   [255, 0, 255],
+// ]
+// WALL_X = 5
+// WALL_Y = 0
+
+
+pub fn main() -> Result<(), String> {
+    let sdl_context = sdl2::init()?;
+    let video_subsystem = sdl_context.video()?;
+
+    let window = video_subsystem
+        .window("rust-tetris", SCREEN_WIDTH, SCREEN_HEIGHT)
+        .position_centered()
+        .opengl()
+        .build()
+        .map_err(|e| e.to_string())?;
+
+    let mut canvas = window.into_canvas().build().map_err(|e| e.to_string())?;
+
+    canvas.set_draw_color(Color::RGB(128, 128, 128));
+    canvas.clear();
+    canvas.present();
+    let mut event_pump = sdl_context.event_pump()?;
+
+    'running: loop {
+        for event in event_pump.poll_iter() {
+            match event {
+                Event::Quit { .. }
+                | Event::KeyDown {
+                    keycode: Some(Keycode::Escape),
+                    ..
+                } => break 'running,
+                _ => {}
+            }
+        }
+
+        canvas.clear();
+        canvas.present();
+        ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 30));
+        // The rest of the game loop goes here...
+    }
+
+    Ok(())
 }
